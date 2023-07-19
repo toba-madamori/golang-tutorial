351b22818cca8d7afcdd3d225226df1bab729f0a
Toba <tobajamezmadamori@gmail.com>
2023-07-19 22:23:44 +0100

    playing around with variables

--- src/main.go
+++ src/main.go
@@ -4,7 +4,16 @@ import (
 	"fmt"
 )
 
-func main() {
-	fmt.Println("Hello Toba")
+var i int = 50
 
+func main() {
+	// var number1 int
+	// number1 = 43
+
+	// var number2 int = 44
+
+	// number := 42
+
+	// fmt.Println(number, number1, number2)
+	fmt.Printf("%v, %T", i, i)
 }
